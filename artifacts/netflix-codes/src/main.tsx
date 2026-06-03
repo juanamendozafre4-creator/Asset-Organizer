5111015ed24766630da01d4d19be196a08d2c78a
juanamendozafre4-creator <juanamendozafre4@gmail.com>
2026-06-02 21:03:18 -0500

    fix: restore setBaseUrl to point API calls to Render backend

--- artifacts/netflix-codes/src/main.tsx
+++ artifacts/netflix-codes/src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { setBaseUrl } from "@workspace/api-client-react";
+
+const API_BASE_URL = "https://netflix-codes-api.onrender.com";
+setBaseUrl(API_BASE_URL);
 
 createRoot(document.getElementById("root")!).render(<App />);
